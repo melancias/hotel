124f2b9162f1be623ec0a945998eb38f184fd12e
Kauany <79481978+melancias@users.noreply.github.com>
2023-03-05 23:53:48 -0300

    Add files via upload

---
+++ reservar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="imagex/png" href="miniicon.png">
+    <title>Obrigada!</title>
+    <link rel="stylesheet" href="form.css" type="text/css">
+</head>
+<body bgcolor:#c6a096>
+<button> <a href="principal.html"> Voltar </a></button>
+<br>
+<br>
+    
+</body>
+</html>
+<?php
+$reserva = $_POST['quartos'];
+
+echo 'Obrigada por reservar o quarto ', $reserva , ' no Erebus Hotel! <br>';
+echo 'Sua senha de identificação para consultar seu quarto é "ABC555" <br>';
+echo 'Aproveite sua estádia!';
+
+
+
+
+
+?>       
